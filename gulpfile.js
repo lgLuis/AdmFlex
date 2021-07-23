f6a9677a16c50be87cd4e3a913b62a78cfd146f4
lgLuis <65200017+lgLuis@users.noreply.github.com>
2021-07-23 02:31:11 -0300

    todo vinculado

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp');
+// const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+
+function css() {
+    return gulp
+        .src('scss/app.scss')
+        .pipe(autoprefixer())
+        .pipe(sass({ outputStyle: 'expanded' }))
+        .pipe(gulp.dest('css'))
+}
+ function wathArchivos(){
+    //  gulp.watch('scss/app.scss', css);
+     gulp.watch('scss/*.scss', css);
+     gulp.watch('index.html');
+
+ }
+
+//tarea
+gulp.task('css', css);
+gulp.task('watch', gulp.parallel(wathArchivos));
